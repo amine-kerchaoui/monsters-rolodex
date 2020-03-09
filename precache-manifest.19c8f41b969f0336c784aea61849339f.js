cc5e6c1aad4c900e119f2c5ae40133e0bcda35ee
amine-kerchaoui <amine.kerchaoui93@gmail.com>
2020-03-09 04:31:54 -0700

    Updates

---
+++ precache-manifest.19c8f41b969f0336c784aea61849339f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912cd43383930336c9073c1979ef3357",
+    "url": "/https//amine-kerchaoui.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "72b70f5e275617761431",
+    "url": "/https//amine-kerchaoui.github.io/monsters-rolodex/static/css/main.302003a6.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/https//amine-kerchaoui.github.io/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/https//amine-kerchaoui.github.io/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b70f5e275617761431",
+    "url": "/https//amine-kerchaoui.github.io/monsters-rolodex/static/js/main.f14ab411.chunk.js"
+  },
+  {
+    "revision": "3774a56d4d1e45d3599c",
+    "url": "/https//amine-kerchaoui.github.io/monsters-rolodex/static/js/runtime-main.96b1d09e.js"
+  }
+]);
